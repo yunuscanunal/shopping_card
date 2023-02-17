afc828c589414d2de74da0c71fb7e71dd95f107a
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-02-17 20:11:15 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
     return (
-        <div className='App'>
-            <Header />
-        </div>
+        <Router>
+            <div className='App'>
+                <Header />
+
+                <Footer />
+            </div>
+        </Router>
     );
 };
 
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <footer class="py-1">
+                <p class="text-center mt-1">
+                    Market App - 2022-2023, All Rights Reserved
+                </p>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
